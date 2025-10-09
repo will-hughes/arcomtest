7b8bbb3deff98c3adb67a12d4f5c1f3c4117ff46
will-hughes <w.p.hughes@reading.ac.uk>
2025-10-09 15:05:55 +0100

    Update taxonomy_table.sql
    Create table

--- archives/arcomt/cfg/sql/taxonomy_table.sql
+++ archives/arcomt/cfg/sql/taxonomy_table.sql
@@ -1,1 +1,12 @@
-
+CREATE TABLE IF NOT EXISTS taxonomy (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    iterm VARCHAR(255) UNIQUE,
+    domain VARCHAR(100),
+    subject VARCHAR(100),
+    facet VARCHAR(100), 
+    lword VARCHAR(255),
+    INDEX (domain),
+    INDEX (subject),
+    INDEX (facet),
+    INDEX (lword)
+);
