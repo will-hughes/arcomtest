183f211851867630136a57433a53a4917c5512a4
will-hughes <w.p.hughes@reading.ac.uk>
2025-10-13 09:24:50 +0100

    Update eprint_fields.pl
    Added descriptive scope

--- archives/arcomt/cfg/cfg.d/eprint_fields.pl
+++ archives/arcomt/cfg/cfg.d/eprint_fields.pl
@@ -160,26 +160,29 @@ push @{$c->{fields}->{eprint}},
     multiple => 1,
     required => 0,
 },
-
 {	
 	name => 'facet',
 	type => 'text',
 	multiple => 1,
 	required => 0,		   
 },
-
 {	
 	name => 'domain',
 	type => 'text',
 	multiple => 1,
 	required => 0,		   
 },
-
 {	
 	name => 'subject',
 	type => 'text',
 	multiple => 1,
 	required => 0,		   
+},
+{
+    name => 'descriptive_scope',
+    type => 'int',  # Store as integer (0-5)
+    required => 0,
+    input_boxes => 1,
 },
 ;
 
