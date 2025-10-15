1c03718bd1f7f76943e37f84039fb161a7e0d055
will-hughes <w.p.hughes@reading.ac.uk>
2025-10-15 11:57:38 +0100

    Update z_local_views.pl
    Revised browse by Journal for clear three-level navigation

--- archives/arcomt/cfg/cfg.d/z_local_views.pl
+++ archives/arcomt/cfg/cfg.d/z_local_views.pl
@@ -3,10 +3,12 @@ push @{$c->{browse_views}},
         id => "journal_volume",
         menus => [
             {
+                id => "publication_menu", # menu ID for better identification
                 fields => [ "publication" ],
                 hideempty => 1,
             },
             {
+                id => "volume_menu", # menu ID for better identification        
                 fields => [ "volume" ],
                 hideempty => 1,
                 group => "publication",
@@ -18,34 +20,61 @@ push @{$c->{browse_views}},
             },
         ],
         render_title => sub {
-            my ($session, $current_view, $menu, $value) = @_;
-            if ($menu->{fields}->[0] eq "publication") {
-                return $session->html_phrase("viewtitle:browse_by_journal");
-            } 
-            elsif ($menu->{fields}->[0] eq "volume") {
-                my $journal = $menu->{group_value} || "Journal";
-                my $dataset = {
-                    journal => $session->make_text($journal),
-                    volume => defined $value ? $session->make_text($value) : undef
-                };
-                return defined $value 
-                    ? $session->html_phrase("viewtitle:browse_volume_contents", %$dataset)
-                    : $session->html_phrase("viewtitle:browse_volumes_of_journal", %$dataset);
-            }
-            return $session->make_text("Browse");
-        },
+        my ($session, $current_view, $menu, $value) = @_;
+        
+        # Debug: Check what we're working with
+        # $session->get_repository->log("Current menu: " . ($menu->{id} || 'no id'));
+        # $session->get_repository->log("Menu fields: " . join(',', @{$menu->{fields}}));
+        
+        # Level 1: No publication selected yet
+        if (!defined $current_view->{menus}->[0]{selected}) {
+            return $session->html_phrase("viewtitle:browse_by_journal");
+        }
+        
+        # Level 2: Publication selected, but no volume selected
+        elsif (!defined $current_view->{menus}->[1]{selected}) {
+            my $journal = $current_view->{menus}->[0]{selected};
+            return $session->html_phrase(
+                "viewtitle:browse_volumes_of_journal",
+                journal => $session->make_text($journal)
+            );
+        }
+        
+        # Level 3: Both publication and volume selected
+        else {
+            my $journal = $current_view->{menus}->[0]{selected};
+            my $volume = $current_view->{menus}->[1]{selected};
+            return $session->html_phrase(
+                "viewtitle:browse_volume_contents", 
+                journal => $session->make_text($journal),
+                volume => $session->make_text($volume)
+            );
+        }
+    },
         render_up_link => sub {
-            my ($session, $current_view, $menu) = @_;
+        my ($session, $current_view, $menu) = @_;
+        
+        # Level 3: Back to volumes list
+        if (defined $current_view->{menus}->[1]{selected}) {
+            return $session->html_phrase("navigation:back_to_volumes");
+        }
+        # Level 2: Back to journals list  
+        elsif (defined $current_view->{menus}->[0]{selected}) {
             return $session->html_phrase("navigation:back_to_journals");
-        },
+        }
+        # Level 1: No up link needed
+        else {
+            return undef;
+        }
+    },
         filters => [
-            { meta_fields => [ "type" ], value => "article" },
-            { meta_fields => [ "publication" ], value => ".+" },
-            { meta_fields => [ "volume" ], value => ".+" },
-        ],
-        order => "number/title",
-        max_items => 10000,
-        variation => [ "DEFAULT;numeric" ],
+        { meta_fields => [ "type" ], value => "article" },
+        { meta_fields => [ "publication" ], value => ".+" },
+        { meta_fields => [ "volume" ], value => ".+" },
+    ],
+    order => "number/title",
+    max_items => 10000,
+    variation => [ "DEFAULT;numeric" ],
 },
 {
    id => "doctype",
