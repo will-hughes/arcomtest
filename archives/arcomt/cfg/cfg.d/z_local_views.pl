82b3c47cb7e303019549aa32ce142abdf93fea09
will-hughes <w.p.hughes@reading.ac.uk>
2025-10-16 15:29:22 +0100

    Clean up menu configuration in z_local_views.pl
    Removed unused menu configuration options from taxonomic view.

--- archives/arcomt/cfg/cfg.d/z_local_views.pl
+++ archives/arcomt/cfg/cfg.d/z_local_views.pl
@@ -45,11 +45,6 @@ push @{$c->{browse_views}},
         menus => [
         {
             fields => [ "iterm" ],
-            new_column_at => [1, 1],
-            mode => "sections",
-            open_first_section => 1,
-            group_range_function => "EPrints::Update::Views::cluster_ranges_30",
-            grouping_function => "group_by_a_to_z_hideempty",
         },
     ],
     order => "creators_name/date",
