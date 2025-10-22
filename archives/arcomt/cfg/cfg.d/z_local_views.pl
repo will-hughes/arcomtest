35ce8aaf96a351935e6b95a603cf152dff697036
Will Hughes <w.p.hughes@reading.ac.uk>
2025-10-22 08:45:52 +0100

    Update z_local_views.pl

--- archives/arcomt/cfg/cfg.d/z_local_views.pl
+++ archives/arcomt/cfg/cfg.d/z_local_views.pl
@@ -102,5 +102,20 @@ push @{$c->{browse_views}},
         }, 
     ],
     order => "creators_name/date", 
+},
+{   id => "decriptive_scope",
+    allow_null => 0,
+    hideempty => 1,
+    menus => [
+        { 
+        fields => [ "decriptive_scope" ], 
+        new_column_at => [1,1],
+        mode => "sections",
+        open_first_section => 1,
+        group_range_function => "EPrints::Update::Views::cluster_ranges_30",
+        grouping_function => "group_by_a_to_z_hideempty",
+        }, 
+    ],
+    order => "creators_name/date", 
 };
 
