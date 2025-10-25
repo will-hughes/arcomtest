2b2002564462589f3b8ce50442a802bd6dceed12
Will Hughes <w.p.hughes@reading.ac.uk>
2025-10-26 00:19:17 +0100

    Create z_database_taxonomy.pl for DB configuration
    Add optional configuration for a separate taxonomy database.

---
+++ archives/arcomt/cfg/cfg.d/z_database_taxonomy.pl
@@ -0,0 +1,6 @@
+# Optional: override for a separate taxonomy DB
+# $c->{taxonomy_db} = {
+#     dsn  => "DBI:mysql:database=taxonomy_db;host=localhost;mysql_enable_utf8=1",
+#     user => "taxonomy_user",
+#     pass => "taxonomy_pass",
+# };
