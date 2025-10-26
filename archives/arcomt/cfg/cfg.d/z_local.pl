1324f1659d7e8827b378a904b037b14fdc791cc4
Will Hughes <w.p.hughes@reading.ac.uk>
2025-10-26 01:02:23 +0100

    Update database credentials in z_local.pl

--- archives/arcomt/cfg/cfg.d/z_local.pl
+++ archives/arcomt/cfg/cfg.d/z_local.pl
@@ -2,7 +2,7 @@
 # This forces the TaxonomyDBHelpers module to use these credentials rather than
 # trying to autodiscover them (useful for the test script and if autodiscovery fails).
 $c->{taxonomy_db} = {
-    dsn  => "DBI:mysql:database=arcom;host=localhost;mysql_enable_utf8=1",
+    dsn  => "DBI:mysql:database=arcomt;host=localhost;mysql_enable_utf8=1",
     user => "eprints",
-    pass => "your_eprints_db_password_here",
+    pass => "FRGmDrtWuaG93J6M",
 };
