81d1293df17d1eafea6ff21210975040a3fdc2e7
Will Hughes <w.p.hughes@reading.ac.uk>
2025-10-26 15:12:12 +0000

    Update find_unused_phrases.pl

--- archives/arcomt/cfg/bin/find_unused_phrases.pl
+++ archives/arcomt/cfg/bin/find_unused_phrases.pl
@@ -7,34 +7,49 @@ use EPrints;
 
 my $repo = EPrints->new->repository( 'arcomt' ) or die "Could not load repository";
 
-# Load all phrases
-my $phrases = $repo->get_phrases();
+# Get the phraseids by loading the phrase files directly
+my $phraseids = {};
 
-# Common phrase patterns to check for removal
+# Load phrases from all phrase files
+my @phrase_files = (
+    '/opt/eprints3/archives/arcomt/cfg/lang/en/phrases/core.xml',
+    '/opt/eprints3/archives/arcomt/cfg/lang/en/phrases/local.xml', 
+    '/opt/eprints3/archives/arcomt/cfg/lang/en/phrases/zz_local.xml',
+    # Add other phrase files as needed
+);
+
+foreach my $file (@phrase_files) {
+    next unless -f $file;
+    open my $fh, '<', $file or next;
+    while (my $line = <$fh>) {
+        if ($line =~ /<epp:phrase\s+id="([^"]+)"/) {
+            $phraseids->{$1} = $file;
+        }
+    }
+    close $fh;
+}
+
+print "Found " . scalar(keys %$phraseids) . " phrases in configuration\n\n";
+
+# Common phrase patterns to check for removal (your compound fields)
 my @patterns_to_check = (
-    'viewname_eprint_facet_',  # All the facet browse views
-    'xmlui_eprint_fieldname_',
-    'xmlui_eprint_fieldhelp_',
-    'xmlui_eprint_fieldopt_',
-    'eprint_fieldname_',
-    'eprint_fieldhelp_',
-    # Add your compound field names here
     'facet_iterm',
     'facet_domain', 
     'domain_subject',
-    'subject_iterm',
+    'subject_iterm', 
     'taxonomy_path',
+    'viewname_eprint_facet_',  # All the facet browse views
 );
 
 print "Checking for potentially unused phrases...\n\n";
 
-foreach my $phrase_id (keys %$phrases) {
+foreach my $phrase_id (sort keys %$phraseids) {
     foreach my $pattern (@patterns_to_check) {
         if ($phrase_id =~ /\Q$pattern\E/) {
-            print "POTENTIALLY UNUSED: $phrase_id\n";
+            print "POTENTIALLY UNUSED: $phrase_id (from $phraseids->{$phrase_id})\n";
             last;
         }
     }
 }
 
-print "\nDone. Review these phrases before deleting.\n";
+print "\nReview these phrases before deleting from your phrase files.\n";
