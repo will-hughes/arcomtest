bf602de677f352208d2038fd747af81f8c136254
Will Hughes <w.p.hughes@reading.ac.uk>
2025-10-28 17:26:39 +0000

    Update z_eprint_search_advanced.pl

--- archives/arcomt/cfg/cfg.d/z_eprint_search_advanced.pl
+++ archives/arcomt/cfg/cfg.d/z_eprint_search_advanced.pl
@@ -15,7 +15,7 @@ $c->{search}->{advanced} =
 		{ meta_fields => [ "subject" ] },
 		{ meta_fields => [ "facet" ] },
 		{ meta_fields => [ "official_url" ] },
-		{ meta_fields => [ "note" ] },
+		{ meta_fields => [ "place_of_pub" ] },
 		{ meta_fields => [ "type" ] },
 	],
 	template => "default",
@@ -63,6 +63,7 @@ License along with EPrints 3.4.
 If not, see L<http://www.gnu.org/licenses/>.
 
 =for LICENSE END
+
 
 
 
